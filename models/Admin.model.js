7135782ec35e8e8651ecf98d7a0469a5ba352360
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2020-03-24 16:20:08 +0100

    lol

---
+++ models/Admin.model.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+
+const AdminSchema=new Schema({
+    username:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+});
+
+const Admin=mongoose.model('Admin',AdminSchema);
+
+module.exports=Admin;
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,24 @@
 const express=require('express');
+const Admin=require('../models/Admin.model');
 
 const router=express.Router();
 
 router.route('/').get((req,res)=>{
 
+});
+
+route.route('/login').post((req,res)=>{
+    const {username,password}=req.body;
+    if(!username || !password){
+        res.status(400).json({msg:'Enter Al fields'});
+    }
+    Admin.findOne({username})
+    .then(user=>{
+        if(!user){
+            res.status.json({msg:'False Credentials'});
+        }
+
+    })
 });
 
 module.exports=router;
