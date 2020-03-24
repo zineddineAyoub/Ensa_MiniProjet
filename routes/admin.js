0aafd5c1ba5a02e117d34ff63b159acb37593c96
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2020-03-24 16:40:12 +0100

    modf server and adding login routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const Admin=require('../models/Admin.model');
+const jwt=require('jsonwebtoken');
+const auth=require('../middleware/auth');
 
 const router=express.Router();
 
@@ -7,7 +9,7 @@ router.route('/').get((req,res)=>{
 
 });
 
-route.route('/login').post((req,res)=>{
+router.route('/login').post((req,res)=>{
     const {username,password}=req.body;
     if(!username || !password){
         res.status(400).json({msg:'Enter Al fields'});
@@ -17,7 +19,31 @@ route.route('/login').post((req,res)=>{
         if(!user){
             res.status.json({msg:'False Credentials'});
         }
+        if(user.password==password){
+            jwt.sign(
+                {id:user._id},
+                'secret',
+                {expiresIn:36000},
+                (err,token)=>{
+                    if(err) throw err;
+                    res.json({
+                        token,
+                        user:{
+                            id:user._id,
+                            username:user.username
+                        }
+                    })
+                }
+            )
+        }
+    })
+});
 
+
+router.get('/user',auth,(req,res)=>{
+    Admin.findById(req.user.id)
+    .then(user=>{
+        res.json(user);
     })
 });
 
---
+++ routes/etudiant.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const Etudiant=require('../models/Etudiant.model');
+const jwt=require('jsonwebtoken');
+const auth=require('../middleware/auth');
+
+const router=express.Router();
+
+router.route('/').get((req,res)=>{
+
+});
+
+router.route('/login').post((req,res)=>{
+    const {cne,cin,password}=req.body;
+    if(!cne || !cin || !password){
+        res.status(400).json({msg:'Enter Al fields'});
+    }
+    Admin.findOne({cne})
+    .then(user=>{
+        if(!user){
+            res.status.json({msg:'False Credentials'});
+        }
+        if(user.cin==cin && user.password==password){
+            jwt.sign(
+                {id:user._id},
+                'secret',
+                {expiresIn:36000},
+                (err,token)=>{
+                    if(err) throw err;
+                    res.json({
+                        token,
+                        user:{
+                            nom:user.nom,
+                            prenom:user.prenom,
+                            cne:user.cne,
+                            cin:user.cin,
+                            email:user.email,
+                            image:user.image,
+                            niveauFiliere:user.niveauFiliere
+                        }
+                    })
+                }
+            )
+        }
+    })
+});
+
+
+router.get('/user',auth,(req,res)=>{
+    Admin.findById(req.user.id)
+    .then(user=>{
+        res.json(user);
+    })
+});
+
+module.exports=router;
