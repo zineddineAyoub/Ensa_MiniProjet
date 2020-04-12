52816c71f9c672585e5a8fef72ea9b143ef7098a
omaratbd7 <58294140+omaratbd7@users.noreply.github.com>
2020-04-12 08:38:26 +0100

    Création du Document.model

---
+++ models/Document.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const Document = new mongoose.Schema({
+    Nom: {
+      type: String
+    },
+
+    Type: {
+      type: String
+    },
+
+   Fichier : {
+    type: String
+   },
+
+
+    matiere: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Matiere'}
+
+    
+  })
+  
+  module.exports = mongoose.model('Document', Document)
